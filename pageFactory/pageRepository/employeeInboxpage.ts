9d7cbda14f1245a651c3928fb2baf052a8ece03b
vasanthkumar.ezhilkumar <vasanthkumar.ezhilkumar@outlook.com>
2025-05-15 23:04:44 +0530

    regression

--- pageFactory/pageRepository/employeeInboxpage.ts
+++ pageFactory/pageRepository/employeeInboxpage.ts
@@ -688,7 +688,6 @@ export class employeeInboxPage extends WebActionsPage {
         await super.click(this.page.locator('[aria-label=' + gender + ']'));
         await super.click(this.page.locator('//div[@data-automation-id="saveButton"]//*[@aria-label="Save Gender"]'));
         await super.click(this.editDob);
-        // await this.page.waitForTimeout(1000);
         await super.setTextWithType(this.page.getByPlaceholder('DD'), dob);
         // await this.page.keyboard.type(dob);
         // await this.page.keyboard.press('Enter');
@@ -706,7 +705,6 @@ export class employeeInboxPage extends WebActionsPage {
         }
         await super.click(this.editmartial);
         await super.setTextWithDoubleEnter(this.martialstatus, martialstat);
-
 
         await super.click(this.editCitizenship);
         await super.selectFromCustomDropDrown(this.citizenship, citizen);
--- tests/WorkDaySuite/WorkdayHires/NewHire_Hungary.test.ts
+++ tests/WorkDaySuite/WorkdayHires/NewHire_Hungary.test.ts
@@ -174,7 +174,7 @@ for (const sheetName in sheetsJson) {
         await appCommon.SuccessEventHandle();
 
 
-        await hrInbxPage.setManageProbationHun("NaN", "NaN");
+        await hrInbxPage.setManageProbation("NaN", "NaN");
         //await capObj.checkForScreenErrors();
         await appCommon.SuccessEventHandle();
 
@@ -243,7 +243,6 @@ for (const sheetName in sheetsJson) {
         await empInboxpage.addAdditionalNameHungary(data.NameType, data.Country, data.GivenName1, data.FamilyName1, data.NameType2);
 
         //let HRPartner = "10559802";
-
         await appCommon.SearchboxEmp("Start Proxy");
         await proxy.startProxy(HRPartner);
         await appCommon.MyTasks();
