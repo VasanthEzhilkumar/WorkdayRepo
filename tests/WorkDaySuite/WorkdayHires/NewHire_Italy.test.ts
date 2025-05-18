44badd604f15f90ff84e3f90f39d679aa748145a
vasanthkumar.ezhilkumar <vasanthkumar.ezhilkumar@outlook.com>
2025-05-19 01:07:40 +0530

    updated code for Hungary

--- tests/WorkDaySuite/WorkdayHires/NewHire_Italy.test.ts
+++ tests/WorkDaySuite/WorkdayHires/NewHire_Italy.test.ts
@@ -17,7 +17,7 @@ let capObj: CaptureAlertErrors;
 
 
 // Define the relative directory path to your Excel file
-const excelFileName = 'Hires/Copy of ItalyREG.xlsx';
+const excelFileName = 'Hires/Workday_NewHire_Italy_Regression_PK17.xlsx';
 const excelFilePath = getExcelFilePath(excelFileName);
 
 // Convert the Excel sheets to JSON format
